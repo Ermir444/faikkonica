f5c9d68cc4ff2522889f53cb2243c1d3542e7aaa
Ermir444 <ermirarifi07@gmail.com>
2025-02-20 00:06:14 +0100

    Server.js

--- Server.js
+++ Server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/v1/', (req, res) => {
+app.get('/api/v1/verify/facebook', (req, res) => {
     res.json({ status: 'success', message: 'Facebook verification successful' });
 });
 
